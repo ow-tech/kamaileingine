32516af9222c17e97e1788f2f3d439104ff70889
israelwangila <wangilaisrael@gmail.com>
2020-09-09 19:26:31 +0300

    styling the foother

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function onSiteAppear(){
-    var introSlogan = document.querySelector('.introSlogan')
-    let introPosition = introSlogan.getBoundingClientRect().top;
-    console.log(introPosition);
-}
-
-onSiteAppear();
-console.log('Alex compuetr')
---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+console.log('Alex compuetr')
+function onSiteAppear(){
+    var introSlogan = document.querySelector('.introSlogan');
+    var introPostion = introSlogan.get;
+    console.log(introSlogan);
+
+}
+onSiteAppear()
