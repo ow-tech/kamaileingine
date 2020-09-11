c315b9529310e56c8e24ace68eca953842c2fc20
ow-tech <wagwanwekon@outlook.com>
2020-09-11 13:35:08 +0300

    fixing of jquery burgs, introduction of slider css

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
 
-// console.log('Alex compuetr')
-// function onSiteAppear(){
-//     var introSlogan = document.querySelector('.introSlogan');
-//     var introPostion = introSlogan.get;
-//     console.log(introSlogan);
+// $(function(){
+//     $('.navis li ').hover(
+//         function(){$('ul.child', this).slideDown(500);
+//     },
+//         }
+//     );
+// });
 
-// }
-// onSiteAppear()
 
 $(function(){
     $('.navis li').hover(
         function(){
             $('ul.child', this).slideDown(500);
         },
-        function(){
-            $('ul.child', this).fadeOut(200)
-            // $('div.section',this).fadeIn(500)
-        },
-    );
+    )
 });
